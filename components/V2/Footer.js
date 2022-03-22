4433c5f157fae05a7f17d098bf06260a3708d375
hec7orci7o <hectortoraltrabajos@gmail.com>
2022-03-22 23:46:07 +0100

    link

--- components/V2/Footer.js
+++ components/V2/Footer.js
@@ -79,7 +79,7 @@ export default function Footer() {
         >
           <button
             onClick={() =>
-              copy("https://asm-editor.vercel.app/", {
+              copy("https://hotasm.vercel.app/", {
                 debug: false,
                 format: "text/plain",
               })
