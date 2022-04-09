bb2d0d931eb27adb47141ffdfe317abfe00ee78a
hec7orci7o <hectortoraltrabajos@gmail.com>
2022-04-10 00:13:22 +0200

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -21,6 +21,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
 
   return (
     <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
       <Script
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
